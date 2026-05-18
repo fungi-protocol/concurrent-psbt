3220f4bcaa2560d3ae2600f27af986689c2715c2
Yuval Kogman <nothingmuch@woobling.org>
2026-05-18 23:48:57 +0000

    flake: crane-based devShell (nightly by default)

---
+++ nix/devshell.nix
@@ -0,0 +1,22 @@
+{
+  perSystem =
+    {
+      config,
+      pkgs,
+      toolchains,
+      ...
+    }:
+    let
+      mkDevShell =
+        craneLib:
+        craneLib.devShell {
+          packages = with pkgs; [
+          ];
+        };
+    in
+    {
+      devShells = builtins.mapAttrs (_: mkDevShell) toolchains // {
+        default = mkDevShell toolchains.nightly;
+      };
+    };
+}
